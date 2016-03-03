f0f92ac705338034627eebf8721a1c01de6cd361
Mark <shaochen206@gmail.com>
2016-03-03 23:01:34 +0800

    print3.4

---
+++ print.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#输出
+#用print()在括号中加入字符串，就可以向屏幕上输出指定的文字。
+print ("hello world")
+print ('hello world')
+#print()函数可以接受多个字符串，用逗号","隔开，就可以练成一串输出：print()会依次打印每个字符串，遇到逗号","会输出一个空格
+print ('The quick brown fox', 'jumps over','the lazy dog')
+print('The quick brown fox' , 'jumps over',   'the lazy dog')
+print('the', 'sa')
+#print()也可以打印整数，或者计算结果：
+print (300)
+print (100 + 200 + 300)
+print (100+200+300)
+print ('100 + 200 =', 100 + 200 )
